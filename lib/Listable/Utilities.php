9d362099284d30b77b9fd4dda0cd802cddecee84
Bobby Bryant <bobby.bryant@10up.com>
2016-12-02 08:34:11 -0500

    create a utils library that contains logic to check for various conditions like isAssociative array

---
+++ lib/Listable/Utilities.php
@@ -0,0 +1,33 @@
+<?php
+namespace Listable;
+
+class Utilities {
+	public static function isAssociative( $array ) {
+
+		foreach ( $array as $key => $value ) {
+			if ( is_string( $key ) ) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public static function isMultidemensional( $array ) {
+		foreach ( $array as $arr ) {
+			if ( is_array( $arr ) ) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static function containsObjects( $array ) {
+		foreach ( $array as $arr ) {
+			if ( is_object( $arr ) ) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
